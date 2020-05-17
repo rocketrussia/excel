c3d3ac8236f0c90e77348dfe92b2fdb38b1f1442
Denis Korneev <veenrock@gmail.com>
2020-05-17 18:55:32 +0400

    [resize] Finish table resize

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -2,8 +2,8 @@ import {DomListener} from '@core/DomListener';
 
 export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
-    super($root, options.listeners);
-    this.name = options.name
+    super($root, options.listeners)
+    this.name = options.name || ''
   }
   // Return component template
   toHTML() {
